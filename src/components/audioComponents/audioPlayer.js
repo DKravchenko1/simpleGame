56bc98bb5e9146979bced78f3df36bc606672120
Kate Satsura <kate.satsura@gmail.com>
2017-12-17 15:14:15 +0300

    Remove debug

--- src/components/audioComponents/audioPlayer.js
+++ src/components/audioComponents/audioPlayer.js
@@ -1,8 +1,6 @@
 import {store} from "../menuComponents/store";
 
-
 const audioPlayer = function(...audio) {
-    console.log(audio);
     for (let i in audio) {
         audio[i].volume = Number(store.getVolume());
         audio[i].play();
